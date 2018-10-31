fe3350fab14373c2824bdd573c33287274cede36
Slobo <sheriff.lobo@gmail.com>
2018-10-31 19:14:25 -0400

    PyParagraph completed

---
+++ Data_analytics_Excercise-3/python-challenge/PyParagraph/main.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+with open("paragraph_2.txt", 'r', newline = '') as para:
+    
+    paraReader = para.read()
+    letterCount = 0
+    
+    for char in paraReader:
+        if char.isalpha():
+            letterCount += 1
+    
+    wordCount = len(re.split(r' ', paraReader))
+    sentenceCount = paraReader.count('.')
+    sentenceLength = round((wordCount / sentenceCount), 1)
+    letterWordCount = round((letterCount / wordCount), 1)
+    
+    print("Paragraph Analysis")
+    print("-----------------")
+    print(f"Approximate Word Count: {wordCount}")
+    print(f"Approximate Sentence Count: {sentenceCount}")
+    print(f"Average Letter Count: {letterWordCount}")
+    print(f"Average Sentence Length: {sentenceLength}")
