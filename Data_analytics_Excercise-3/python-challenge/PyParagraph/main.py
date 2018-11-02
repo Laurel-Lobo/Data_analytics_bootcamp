256876bbf5ddcc50851e17e9594eba6b45f7df4b
Slobo <sheriff.lobo@gmail.com>
2018-11-01 20:28:35 -0400

    Fixed PyParagraph word and sentence count contingencies

--- Data_analytics_Excercise-3/python-challenge/PyParagraph/main.py
+++ Data_analytics_Excercise-3/python-challenge/PyParagraph/main.py
@@ -1,17 +1,30 @@
 import os
 import re
 
-with open("paragraph_2.txt", 'r', newline = '') as para:
+with open("paragraph_1.txt", 'r') as para:
     
     paraReader = para.read()
     letterCount = 0
+    wordCount = 0
+    sentenceCount = 0
     
     for char in paraReader:
         if char.isalpha():
             letterCount += 1
     
-    wordCount = len(re.split(r' ', paraReader))
-    sentenceCount = paraReader.count('.')
+    words = re.split(r" ", paraReader)
+
+    for word in words:
+        wordCount += 1
+        if "\n\n" in word:
+            wordCount +=1
+        #to account for initials inflating sentenceCount
+        if "." in word and len(word) == 2:
+            sentenceCount -= 1
+            
+    #wordCount = len(re.split(r' ', paraReader))
+    sentenceCount += paraReader.count('.') + paraReader.count('?') + paraReader.count('!')
+    
     sentenceLength = round((wordCount / sentenceCount), 1)
     letterWordCount = round((letterCount / wordCount), 1)
     
