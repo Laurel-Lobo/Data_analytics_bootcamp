9f4ca8090e1399386e71649f87a8a16814e1803a
Slobo <sheriff.lobo@gmail.com>
2018-11-17 14:12:08 -0500

    setup matplot homework

--- Data_analytics_Excercise-3/python-challenge/PyBank/main.py
+++ Data_analytics_Excercise-3/python-challenge/PyBank/main.py
@@ -1,4 +1,4 @@
-import os
+
 import csv
 
 bankData = []
@@ -6,8 +6,8 @@ months = []
 profitsLosses = []
 AvgChange = 0
 
-with open("budget_data.csv", 'r', newline = "", encoding = 'utf-8') as bankFile:
-    bankReader = csv.reader(bankFile, delimiter = ",")
+with open("budget_data.csv", 'r', newline="", encoding='utf-8') as bankFile:
+    bankReader = csv.reader(bankFile, delimiter=",")
     next(bankReader, None)
     
     for line in bankReader:
@@ -16,7 +16,7 @@ with open("budget_data.csv", 'r', newline = "", encoding = 'utf-8') as bankFile:
         
     TMonths = len(months)
     
-    #calculates total across months
+    # calculates total across months
     Total = sum(i for i in profitsLosses)
     
     ChangeCount = (len(profitsLosses) - 1)
@@ -29,25 +29,25 @@ with open("budget_data.csv", 'r', newline = "", encoding = 'utf-8') as bankFile:
 
     CumChange = round((AvgChange/ChangeCount), 2)
     
-    #outputs maximum value and corresponding month
+    # outputs maximum value and corresponding month
     Max = max(profitsLosses)
     MonthMax = months[int(profitsLosses.index(Max))]
     
-    #outputs minimum value and corresponding month
+    # outputs minimum value and corresponding month
     Min = min(profitsLosses)
     MonthMin = months[int(profitsLosses.index(Min))]
     
-    #print to terminal
-    print ("Financial Analysis")
-    print ("----------------------------")
-    print (f"Total Months: {TMonths}")
-    print (f"Total: ${Total}")
-    print (f"Average Change: ${CumChange}")
-    print (f"Greatest Increase in Profits: {MonthMax} (${Max})")
-    print (f"Greatest Decrease in Profits: {MonthMin} (${Min})")
+    # print to terminal
+    print("Financial Analysis")
+    print("----------------------------")
+    print(f"Total Months: {TMonths}")
+    print(f"Total: ${Total}")
+    print(f"Average Change: ${CumChange}")
+    print(f"Greatest Increase in Profits: {MonthMax} (${Max})")
+    print(f"Greatest Decrease in Profits: {MonthMin} (${Min})")
     
-    #print to output file
-    with open ("FinancialAnalysis.txt", "w") as file:
+    # print to output file
+    with open("FinancialAnalysis.txt", "w") as file:
         file.write("Financial Analysis\n")
         file.write("----------------------------\n")
         file.write(f"Total Months: {TMonths}\n")
