c2f2794a98888520261ea69ed8fbc7e9f8237627
Slobo <sheriff.lobo@gmail.com>
2019-04-13 08:06:54 -0400

    Final push bc2019

---
+++ Data_analytics_Excercise_Final/Wine AI/Wine_prof/static/js/app.js
@@ -0,0 +1,66 @@
+// from lcboscrape.js
+var tableData = data;
+
+// Connect to index table
+var tbody = d3.select("tbody");
+
+function buildTable(data) {
+  // Clearing existing data
+  tbody.html("");
+
+  //append row with values for each entry in database
+  data.forEach((dataRow) => {
+   
+    var row = tbody.append("tr");
+
+    Object.values(dataRow).forEach((val) => {
+      var cell = row.append("td");
+      cell.text(val);
+    });
+  });
+}
+
+// Keep Track of all filters
+var filters = {};
+
+function updateFilters() {
+
+  // Save the element, value, and id of the filter that was changed
+  var changedElement = d3.select(this).select("input");
+  var elementValue = changedElement.property("value");
+  var filterId = changedElement.attr("id");
+
+  // If a filter value was entered then add that filterId and value
+  // to the filters list. Otherwise, clear that filter from the filters object
+  if (elementValue) {
+    filters[filterId] = elementValue;
+  }
+  else {
+    delete filters[filterId];
+  }
+
+  // Call function to apply all filters and rebuild the table
+  filterTable();
+
+}
+
+function filterTable() {
+
+  // Set the filteredData to the tableData
+  let filteredData = tableData;
+
+  // Loop through all of the filters and keep any data that
+  // matches the filter values
+  Object.entries(filters).forEach(([key, value]) => {
+    filteredData = filteredData.filter(row => row[key] === value);
+  });
+
+  // Finally, rebuild the table using the filtered Data
+  buildTable(filteredData);
+}
+
+// Attach an event to listen for changes to each filter
+d3.selectAll(".filter").on("change", updateFilters);
+
+// Build the table when the page loads
+buildTable(tableData);
